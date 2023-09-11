34b111d53fdf3b93a1e50ce3ca1d0926a215a869
최원 <79261281+won9230@users.noreply.github.com>
2023-09-11 17:40:23 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    cout << n + m << "\n";
+    cout << n - m << "\n";
+    cout << n * m << "\n";
+    cout << n / m << "\n";
+    cout << n % m << "\n";
+}
