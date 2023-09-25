4f0ecadce18cb77c6ec1dc3575d4457db157188a
최원 <79261281+won9230@users.noreply.github.com>
2023-09-25 19:01:28 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> a[101];
+bool check[101];
+int cnt = 0;
+
+void dfs(int x){
+    check[x] = true;
+    for (int i = 0; i < a[x].size(); i++)
+    {
+        int y = a[x][i];
+        if(!check[y]){
+            dfs(y);
+            cnt++;
+        }
+    }
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u,v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    dfs(1);
+    cout << cnt << endl;    
+}
