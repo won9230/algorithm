9538b31492a14fc89bb3fa86ed0dfdb7e9348b26
최원 <79261281+won9230@users.noreply.github.com>
2024-05-03 18:11:02 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), greater<int>());
+    for(int i = m - 1; i < score.size(); i += m){  
+        answer += score[i] * m;
+    }    
+    
+    return answer;
+}
