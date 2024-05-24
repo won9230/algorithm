6a20d9431af4aa9a91d1387df003dfe632c302d6
최원 <79261281+won9230@users.noreply.github.com>
2024-05-24 19:47:42 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 18:10:59
+2024년 05월 24일 19:47:39
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -7,10 +7,19 @@ using namespace std;
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    sort(score.begin(), score.end(), greater<int>());
-    for(int i = m - 1; i < score.size(); i += m){  
-        answer += score[i] * m;
-    }    
-    
+    vector<int> tmps;
+    sort(score.begin(),score.end(),greater<int>());
+
+    for (int i = 0; i < score.size(); i++)
+    {
+        int tmp = score[i];
+        tmps.push_back(tmp);
+        if(tmps.size() >= m)
+        {
+            int small = *min_element(tmps.begin(),tmps.end());
+            answer += small * m;
+            tmps.clear();
+        }
+    }
     return answer;
 }
