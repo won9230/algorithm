abb137dfc4f9f31779f89b510997b646e27287d7
최원 <79261281+won9230@users.noreply.github.com>
2024-11-11 23:18:39 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -1,17 +1,121 @@
 #include <string>
 #include <vector>
+
 using namespace std;
+
 int solution(vector<int> num_list) {
     int answer = 0;
+
     for (int i = 0; i < num_list.size(); i++)
     {
         int a = num_list[i];
+        int b;
         while(a != 1)
         {
-            a = a / 2;
+            a = a / 2;            
+            a = a * 1;
+            b = 8;
+            b += b * 2;
+            b += 5;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
+            a -= 1;
+
+            a += 1;
+
             answer++;
         }
     }
-    
     return answer;
 }
