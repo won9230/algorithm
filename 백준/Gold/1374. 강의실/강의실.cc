e8cc8aa80184d983b6c6c21524e89bf215bd2c07
최원 <79261281+won9230@users.noreply.github.com>
2024-11-16 01:57:33 +0900

    [Gold V] Title: 강의실, Time: 144 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.cc
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -4,33 +4,39 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
 
-    vector<pair<long long, int>> times;
+    vector<long long> startTimes(n), endTimes(n);
 
-    // 강의 시작 시간과 종료 시간을 구분해 저장
+    // 입력 받기
     for (int i = 0; i < n; i++) {
         long long idx, start, end;
         cin >> idx >> start >> end;
-        times.push_back({start, 1});  // 시작 시간
-        times.push_back({end, -1});  // 종료 시간
+        startTimes[i] = start;
+        endTimes[i] = end;
     }
 
-    // 시간 순으로 정렬, 같은 시간일 경우 종료를 먼저 처리
-    sort(times.begin(), times.end(), [](const pair<long long, int>& a, const pair<long long, int>& b) {
-        if (a.first == b.first) return a.second < b.second;
-        return a.first < b.first;
-    });
+    // 시작 시간과 종료 시간을 정렬
+    sort(startTimes.begin(), startTimes.end());
+    sort(endTimes.begin(), endTimes.end());
 
-    int current = 0, maxRooms = 0;
+    // 두 포인터로 강의실 추적
+    int rooms = 0, maxRooms = 0;
+    int startIdx = 0, endIdx = 0;
 
-    // 시작/종료 이벤트 순회
-    for (const auto& time : times) {
-        current += time.second;  // 시작이면 +1, 종료이면 -1
-        maxRooms = max(maxRooms, current);  // 최대 강의실 개수 갱신
+    while (startIdx < n) {
+        if (startTimes[startIdx] < endTimes[endIdx]) {
+            // 강의가 시작되면 강의실 추가
+            rooms++;
+            maxRooms = max(maxRooms, rooms);
+            startIdx++;
+        } else {
+            // 강의가 끝나면 강의실 반환
+            rooms--;
+            endIdx++;
+        }
     }
 
     cout << maxRooms << endl;
