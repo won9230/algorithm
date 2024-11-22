58327df0654c954deeb8079cb1a2c11f38441081
최원 <79261281+won9230@users.noreply.github.com>
2024-11-23 01:02:20 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n % 2)
+    {
+        cout << "SK";
+    }
+    else
+    {
+        cout << "CY";
+    }
+}
